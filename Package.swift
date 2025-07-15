0e60532be40a969216f79cff3ff8bf0186f33b5d
Anthony Miller <anthonymdev@gmail.com>
2025-07-15 13:57:26 -0700

    Update Tuist to 4.55.6
    WIP: Update Tests to strict concurrency
    
    Update project deployment targets to apollo 2.0 versions
    
    Update configuration to new reccommended values
    
    Minor fix
    
    Swift 6 #file -> #filePath fix
    
    Update SPM Packages to swift-tools-version 6.1
    
    Move Design notes to dev from apollo-ios
    
    Update tuist github action to use mise
    
    Explicitly add swift v6 to Package files

--- Package.swift
+++ Package.swift
@@ -1,7 +1,4 @@
-// swift-tools-version:5.9
-//
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-// Swift 5.9 is available from Xcode 15.0.
+// swift-tools-version:6.1
 
 import PackageDescription
 
@@ -35,7 +32,11 @@ let package = Package(
         .product(name: "ApolloAPI", package: "apollo-ios"),
         .product(name: "OrderedCollections", package: "swift-collections"),
       ],
-      swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
+      swiftSettings: [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableExperimentalFeature("StrictConcurrency")
+      ]
     ),
-  ]
+  ],
+  swiftLanguageModes: [.v6, .v5]
 )
